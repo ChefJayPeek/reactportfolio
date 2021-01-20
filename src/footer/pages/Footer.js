06da170494b3c3844bc75040540b6d833b7c0bd5
Jay Peek <73311799+ChefJayPeek@users.noreply.github.com>
2021-01-20 15:13:14 -0600

    Fine tuning css

--- src/footer/pages/Footer.js
+++ src/footer/pages/Footer.js
@@ -5,7 +5,7 @@ import React from 'react';
 const Footer = () => {
     return (
             // setup the footer
-            <footer className="bg-light text-center text-lg-start">
+            <footer className="bg-light text-center text-sm-start">
             {/*  Copyright */}
             <div className="text-center p-3">
               © 2021 Copyright:
--- src/header/pages/Header.js
+++ src/header/pages/Header.js
@@ -1,25 +1,37 @@
 import React from 'react';
-
-
+import { NavLink } from 'react-router-dom';
+import './Header.css';
 
 const Header = () => {
+    
     return (
             // setup the navbar
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <h2 className="navbar-brand">Jay Peek</h2>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-    
-      <div className="col-12 navbar-nav d-flex justify-content-end">
-        <a className="nav-link" href="/contact">Contact <span className="sr-only">(current)</span></a>
-        <a className="nav-link" href="/portfolio">Portfolio</a>
-        <a className="nav-link active" href="/">About</a>
-      </div>
-    </div>
-  </nav>
-    )
+                <nav className="navbar">
+                <a className="navbar-brand">Jay Peek</a>
+                  <NavLink
+                    exact
+                    activeClassName="navbar__link--active"
+                    className="navbar__link"
+                    to="/"
+                  >
+                    About
+                  </NavLink>
+                  <NavLink
+                    activeClassName="navbar__link--active"
+                    className="navbar__link"
+                    to="/portfolio"
+                  >
+                    Portfolio
+                  </NavLink>
+                  <NavLink
+                    activeClassName="navbar__link--active"
+                    className="navbar__link"
+                    to="/contact"
+                  >
+                    Contact
+                  </NavLink>
+                </nav>
+              );
 };
 
 export default Header;
